1740f4ef97da23d889ef828ec342b922d345c709
maryanibrahim <maryanibrahim301@yahoo.com>
2023-06-18 22:52:42 +0000

    “wordsearch“

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+/* eslint-disable no-plusplus */
+/* eslint-disable no-restricted-syntax */
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(''));
 
-module.exports = wordSearch
+  // check each row for word
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  // transpose the rows to columns
+  let verticalJoin = '';
+  for (let col = 0; col < letters[0].length; col++) {
+    for (let row = 0; row < letters.length; row++) {
+      verticalJoin += letters[row][col];
+    }
+  }
+  // check each new row for word
+  for (const v of verticalJoin) {
+    if (v.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
